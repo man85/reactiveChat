7c60c7c320d6251788c77124e4ea1ec259170454
man85 <some@email.org>
2022-06-27 10:58:18 +0300

    Added password encoder for saving password

--- src/main/java/com/reactiveexample/reactiveChat/service/UserService.java
+++ src/main/java/com/reactiveexample/reactiveChat/service/UserService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -20,6 +21,8 @@ public class UserService implements ReactiveUserDetailsService {
     private static final int USER_ROLE_ID = 2;
 
     private final UserRepository userRepository;
+
+    private final PasswordEncoder encoder;
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
@@ -34,6 +37,7 @@ public class UserService implements ReactiveUserDetailsService {
 
     public Mono<User> addUser(final User user) {
         user.setRoleId(USER_ROLE_ID);
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.findByUsernameWithQuery(user.getUsername()).flatMap((el) -> Mono.<User>error(new UserAlreadyExistsException(user.getUsername()))
         ).switchIfEmpty(userRepository.save(user));
     }
